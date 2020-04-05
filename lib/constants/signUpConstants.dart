f6899dea21baed0b4a3abdb1512b293993c11078
Matthew Slauson <slauson.matthew@gmail.com>
2020-04-05 11:56:33 -0500

    add constants

--- lib/constants/signUpConstants.dart
+++ lib/constants/signUpConstants.dart
@@ -1,8 +1,29 @@
 class SignUpConstants{
+  //label text
   static const String USERNAME = "Username";
   static const String FIRST_NAME = "First Name";
-  static const String MIDDLE_NAME = "Middle Initial";
+  static const String MIDDLE_INITIAL = "Middle Initial";
   static const String LAST_NAME = "Last Name";
   static const String PHONE = "Phone Number";
   static const String EMAIL_ADDRESS = "Email Address";
+  static const String PASSWORD = "Password";
+
+  //hint text
+  static const String USERNAME_HINT = "Please choose a username";
+  static const String FIRST_NAME_HINT = "Please enter your first name";
+  static const String MIDDLE_INITIAL_HINT = "Please enter your middle initial (Optional)";
+  static const String LAST_NAME_HINT = "Please enter your last name";
+  static const String PHONE_HINT = "Please enter your phone number";
+  static const String EMAIL_ADDRESS_HINT = "Please enter your email address";
+  static const String PASSWORD_HINT = "Please choose a password";
+
+
+  //Invalid Messages
+  static const String INVALID_USERNAME = "Invalid Username";
+  static const String INVALID_EMAIL = "Invalid Email Address";
+  static const String INVALID_FIRST_NAME = "Invalid First Name";
+  static const String INVALID_MIDDLE_INITIAL = "Invalid Middle Initial";
+  static const String INVALID_LAST_NAME = "Invalid Last Name";
+  static const String INVALID_PHONE = "Invalid Username";
+  static const String INVALID_PASSWORD = "INVALID_PASSWORD";
 }
