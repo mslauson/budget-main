7169e34a90ea4a105a73a98e76d3ada0a04a8345
Matthew Slauson <slauson.matthew@gmail.com>
2020-04-11 16:45:42 -0500

    added urls for okta

--- lib/constants/UrlConstants.dart
+++ lib/constants/UrlConstants.dart
@@ -1,8 +1,8 @@
 class UrlConstants{
-  static const String OTKA_BASE_DEV = "https://dev-501086.okta.com";
+  static const String OKTA_BASE_DEV = "https://dev-501086.okta.com";
 
 
   //URIS
-  static const String OTKA_ADD_USER = "/api/v1/users?activate=false";
+  static const String OKTA_ADD_USER = "/api/v1/users?activate=false";
 
 }
