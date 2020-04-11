4e7f4775344bb1f352136b0b95182470a3d88cd2
Matthew Slauson <slauson.matthew@gmail.com>
2020-04-11 16:18:11 -0500

    addedConstants

--- lib/constants/UrlConstants.dart
+++ lib/constants/UrlConstants.dart
@@ -1,4 +1,8 @@
 class UrlConstants{
   static const String OTKA_BASE_DEV = "https://dev-501086.okta.com";
 
+
+  //URIS
+  static const String OTKA_ADD_USER = "/api/v1/users?activate=false";
+
 }
---
+++ lib/constants/oktaConstants.dart
@@ -0,0 +1,3 @@
+class OktaConstants{
+  static const Map<String, String> OTIKA_DEV_HEDERS = {"Content-type": "application/json", "Authorizaton": "SSWS 001w_2qOzzeGCasIDjJ33JQp5CZDE7-iIhrrPaqz__"};
+}
