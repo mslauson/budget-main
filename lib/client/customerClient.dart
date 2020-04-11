55cc6dca026a77df69510201df9318ff020283ef
Matthew Slauson <slauson.matthew@gmail.com>
2020-04-11 16:44:51 -0500

    update to use error handler

--- lib/client/customerClient.dart
+++ lib/client/customerClient.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
 import 'package:main/constants/signUpConstants.dart';
+import 'package:main/error/errorHandler.dart';
 
 class CustomerClient {
   Future<String> addCustomer(String payload) async {
@@ -13,16 +13,12 @@ class CustomerClient {
 
     var response = await post(url, headers: headers, body: payload);
     if(response.statusCode != 200){
-      onError(response, "Registration");
+      ErrorHandler.onError(response, "Registration");
     }
     return response.body;
   }
 
-  onError(Response response, String context){
-    log(context + "failed with status code: "+response.statusCode.toString()+" response: "+response.body+"");
-    throw context + " failed.";
-  }
-
+  
   Future<bool> checkUserName(String payload)  async {
     String url =
         'http://customer-microservice-dev.blossombudgeting.io/api/v1/customer/validate/' + payload;
@@ -30,7 +26,7 @@ class CustomerClient {
 
     var response = await get(url, headers: headers );
     if(response.statusCode != 200){
-      onError(response, "Username Verification");
+      ErrorHandler.onError(response, "Username Verification");
     }
     var booleanResponse = jsonDecode(response.body);
     return booleanResponse[SignUpConstants.USERNAME_TAKEN_KEY];
