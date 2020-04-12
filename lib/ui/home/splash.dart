57efd6b145c46e0f8bf9d1b15699c8bc24504c1d
Matthew Slauson <slauson.matthew@gmail.com>
2020-04-12 00:28:19 -0500

    make stateless

--- lib/ui/home/splash.dart
+++ lib/ui/home/splash.dart
@@ -1,26 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:main/ui/signUp/signup.dart';
 
-class Splash extends StatefulWidget {
+class Splash extends StatelessWidget {
   Splash({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
-  @override
-  SplashState createState() => SplashState();
-}
-
-class SplashState extends State<Splash> {
-  @override
+   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
@@ -38,20 +24,6 @@ class SplashState extends State<Splash> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const SizedBox(height: 30),
