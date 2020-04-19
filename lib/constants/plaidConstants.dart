1937f9847ee813f8206eccb406061b8b5ef1e709
mslauson <slauson.matthew@gmail.com>
2020-04-19 15:06:29 -0500

    add more constants

--- lib/constants/plaidConstants.dart
+++ lib/constants/plaidConstants.dart
@@ -1,6 +1,6 @@
 class PlaidConstants{
   //product
-  static const String IDENTITY_PRODUCT = "Identity";
+  static const String IDENTITY_PRODUCT = "identity,auth";
 
   //env
   static const String ENV = "development";
@@ -9,5 +9,16 @@ class PlaidConstants{
   static const String CLIENT_ID = "5e3e3876ba7c950013d677b8";
   static const String PUBLIC_KEY = "8b40e105420880d019d286cc4553e1";
   static const String CLIENT_SECRET = "e3203264e267e1ec00cd35185bee10";
+
+  //client name
+  static const String CLIENT_NAME = "Blossom";
+
+  //connectionUrl
+  static const String  PLAID_LINK_URL = "https://cdn.plaid.com/link/v2/stable/link.html" +
+  "?isWebview=true" + 
+  "&key="+PUBLIC_KEY+
+  "&env="+ENV+
+  "&product="+IDENTITY_PRODUCT+
+  "&clientName="+CLIENT_NAME;
 
 }
