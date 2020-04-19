02c5d4e7225e646d412dbfc4509f264a687111b0
mslauson <slauson.matthew@gmail.com>
2020-04-19 13:52:31 -0500

    secure home draft

---
+++ lib/ui/secureHome/secureHome.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SecureHome extends StatefulWidget {
+  SecureHome({Key key}) : super(key: key);
+
+  @override
+  _SecureHomeState createState() => _SecureHomeState();
+}
+
+
+class _SecureHomeState extends State<SecureHome> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: ,
+    body: Container(
+       child: CustomScrollView(
+         slivers: <Widget>[
+           Center(child: _widgetOptions.elementAt(_selectedIndex))
+         
+         ],
+       ),
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Dashboard'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance),
+            title: Text('Accounts'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
