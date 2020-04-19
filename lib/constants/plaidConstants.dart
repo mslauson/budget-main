c4f2c89746cb4a3a1993daf686061a3de8b59216
mslauson <slauson.matthew@gmail.com>
2020-04-19 14:54:14 -0500

    add plaid constants

---
+++ lib/constants/plaidConstants.dart
@@ -0,0 +1,13 @@
+class PlaidConstants{
+  //product
+  static const String IDENTITY_PRODUCT = "Identity";
+
+  //env
+  static const String ENV = "development";
+
+  //credentials
+  static const String CLIENT_ID = "5e3e3876ba7c950013d677b8";
+  static const String PUBLIC_KEY = "8b40e105420880d019d286cc4553e1";
+  static const String CLIENT_SECRET = "e3203264e267e1ec00cd35185bee10";
+
+}
