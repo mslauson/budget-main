a52b8803048e7de0684b757a6b89d6a86c5baff5
mslauson <slauson.matthew@gmail.com>
2020-05-17 06:58:52 -0500

    added client for logging in customer

--- lib/client/customerClient.dart
+++ lib/client/customerClient.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:main/constants/customerMicroserviceConstants.dart';
 import 'package:main/constants/iamConstants.dart';
-
 import 'package:main/error/errorHandler.dart';
+import 'package:main/model/iam/blossomLoginResponse.dart';
 
 class CustomerClient {
   Future<String> addCustomer(String payload) async {
@@ -18,17 +19,31 @@ class CustomerClient {
     return response.body;
   }
 
-  
-  Future<bool> checkUserName(String payload)  async {
+  Future<bool> checkUserName(String payload) async {
     String url =
-        'http://customer-microservice-dev.blossombudgeting.io/api/v1/customer/validate/' + payload;
+        'http://customer-microservice-dev.blossombudgeting.io/api/v1/customer/validate/' +
+            payload;
     Map<String, String> headers = {"Content-type": "application/json"};
 
-    var response = await get(url, headers: headers );
-    if(response.statusCode != 200){
+    var response = await get(url, headers: headers);
+    if (response.statusCode != 200) {
       ErrorHandler.onError(response, "Username Verification");
     }
     var booleanResponse = jsonDecode(response.body);
     return booleanResponse[IAMConstants.USERNAME_TAKEN_KEY];
+  }
+
+  static Future<BlossomLoginResposne> loginUser(String email) async {
+    String url = CustomerMicroserviceConstants.BASE_URL +
+        CustomerMicroserviceConstants.BASE_URI +
+        email +
+        CustomerMicroserviceConstants.LOGIN_URI;
+    Map<String, String> headers = {"Content-type": "application/json"};
+
+    var response = await get(url, headers: headers);
+    if (response.statusCode != 200) {
+      ErrorHandler.onError(response, "Customer Login");
+    }
+    return BlossomLoginResposne.fromJson(jsonDecode(response.body));
   }
 }
---
+++ lib/constants/customerMicroserviceConstants.dart
@@ -0,0 +1,6 @@
+class CustomerMicroserviceConstants {
+  static const BASE_URL =
+      "http://customer-microservice-dev.blossombudgeting.io";
+  static const BASE_URI = "/api/v1/customer/";
+  static const LOGIN_URI = "/login";
+}
