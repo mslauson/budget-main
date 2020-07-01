3db697f587561fc2ea18c60b85399e87468d0b53
Matthew Slauson <slauson.matthew@gmail.om>
2020-06-30 20:12:23 -0500

    update registrationService

--- lib/service/registrationService.dart
+++ lib/service/registrationService.dart
@@ -7,23 +7,23 @@ import 'package:main/models/iam/signUpForm.dart';
 import 'package:main/models/valueModel.dart';
 
 class RegistrationService {
-  Future<bool> addCustomer(bool validForm, SignUpForm signUpForm,
+  final _auth = FirebaseAuth.instance;
+   Future<bool> registerToFirebase( SignUpForm signUpForm,
       ValueModel valueModel) async {
-    final _auth = FirebaseAuth.instance;
-    if (validForm) {
-      CustomerClient customerClient = new CustomerClient();
-      String customerResponse =
-      await customerClient.addCustomer(jsonEncode(signUpForm.toJson()));
-      log(customerResponse.toString());
-      final AuthResult authResult = await _auth.createUserWithEmailAndPassword(
-        email: signUpForm.emailAddress,
-        password: valueModel.value,
-      );
-      _auth.c
-      log(authResult.additionalUserInfo.username +
-          " has been registered successfully");
-      return true;
-    }
-    return false;
+    final AuthResult authResult = await _auth.createUserWithEmailAndPassword(
+      email: signUpForm.emailAddress,
+      password: valueModel.value,
+    );
+    log(authResult.additionalUserInfo.username +
+        " has been registered successfully");
+    await addCustomer(signUpForm);
+    return true;
   }
+   Future<void> addCustomer( SignUpForm signUpForm) async {
+     CustomerClient customerClient = new CustomerClient();
+     String customerResponse =
+     await customerClient.addCustomer(jsonEncode(signUpForm.toJson()));
+     log(customerResponse.toString());
+
+   }
 }
