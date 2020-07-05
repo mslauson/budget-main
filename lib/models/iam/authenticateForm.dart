a4ac3d5ebe5b75eccd3f01258e243d59d2530052
Devlin Verley <verley93@gmail.com>
2020-07-04 21:47:21 -0500

    Remove getters/setters

--- lib/models/iam/authenticateForm.dart
+++ lib/models/iam/authenticateForm.dart
@@ -1,26 +1,21 @@
 class AuthenticationForm {
-  String _username;
-  String _password;
+  String username;
+  String password;
 
   AuthenticationForm({String username, String password}) {
-    this._username = username;
-    this._password = password;
+    this.username = username;
+    this.password = password;
   }
 
-  String get username => _username;
-  set username(String username) => _username = username;
-  String get password => _password;
-  set password(String password) => _password = password;
-
   AuthenticationForm.fromJson(Map<String, dynamic> json) {
-    _username = json['username'];
-    _password = json['password'];
+    username = json['username'];
+    password = json['password'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['username'] = this._username;
-    data['password'] = this._password;
+    data['username'] = this.username;
+    data['password'] = this.password;
     return data;
   }
 }
