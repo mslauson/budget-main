09b3237e45b9fd1ce98e552a3bfc75b0a2de6c67
Devlin Verley <verley93@gmail.com>
2020-07-04 21:49:12 -0500

    Add theme components

---
+++ lib/theme/blossomColors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class BlossomColors {
+  static final Color darkShade = Color(0xff01233F);
+  static final Color darkAccent = Color(0xff81798C);
+  static final Color primary = Color(0xff664264);
+  static final Color lightAccent = Color(0xff6C9191);
+  static final Color lightShade = Color(0xfff6948c);
+}
