e6fd625e0cf72263202d40aedd5d67952fc48d66
mslauson <slauson.matthew@gmail.com>
2020-07-20 20:20:22 -0500

    add constants for validating customer

--- lib/constants/customerMicroserviceConstants.dart
+++ lib/constants/customerMicroserviceConstants.dart
@@ -2,4 +2,6 @@ class CustomerMicroserviceConstants {
   static const BASE_URL_CUSTOMERS = "http://dev.blossombudgeting.io/customers";
   static const ENDPOINT_V1_CUSTOMERS = "/api/v1/";
   static const ENDPOINT_SUFFIX_VALIDATE = "validate/";
+  static const ENDPOINT_SUFFIX_EMAIL = "email/";
+  static const ENDPOINT_SUFFIX_PHONE = "phone/";
 }
