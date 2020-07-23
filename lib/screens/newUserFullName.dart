f8f134307df161551d7d029e2aad3bbde7e6487c
mslauson <slauson.matthew@gmail.com>
2020-07-22 20:12:50 -0500

    finish gathering full name

--- lib/screens/newUserFullName.dart
+++ lib/screens/newUserFullName.dart
@@ -1,32 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:main/models/global/activeUser.dart';
 import 'package:main/models/iam/authenticationModel.dart';
+import 'package:main/models/iam/signUpForm.dart';
+import 'package:main/screens/splash.dart';
 import 'package:main/service/authenticationService.dart';
+import 'package:main/service/registrationService.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class NewUserFullName extends StatelessWidget {
-  String _lastLogin = '';
+  final String phone;
+
+  NewUserFullName(this.phone);
 
   @override
   Widget build(BuildContext context) {
-    AuthenticationModel authenticationModel = new AuthenticationModel();
-    AuthenticationService _authService = new AuthenticationService();
+    TextEditingController _fullNameController = new TextEditingController();
     bool validForm;
     return Scaffold(
       body: ScopedModelDescendant<ActiveUser>(
           builder: (BuildContext context, Widget child, ActiveUser model) {
-        return new Container(
-            padding: const EdgeInsets.all(30.0),
-            color: Colors.white,
-            child: new Container(
-              child: new Center(
-                  child: new Column(children: [
+            return new Container(
+                padding: const EdgeInsets.all(30.0),
+                color: Colors.white,
+                child: new Container(
+                  child: new Center(
+                      child: new Column(children: [
                 new Padding(padding: EdgeInsets.only(top: 140.0)),
                 new Text(
-                  'Please Provide Blossom With Name',
-                  style: new TextStyle(color: Colors.black, fontSize: 25.0),
+                  'Please Provide Blossom With Your Name',
+                  style: new TextStyle(color: Colors.blue, fontSize: 25.0),
                 ),
                 new Padding(padding: EdgeInsets.only(top: 50.0)),
                 new TextFormField(
@@ -37,12 +42,17 @@ class NewUserFullName extends StatelessWidget {
                       borderRadius: new BorderRadius.circular(25.0),
                       borderSide: new BorderSide(),
                     ),
-                    //fillColor: Colors.green
                   ),
+                  controller: _fullNameController,
                   validator: (val) {
                     if (val.length == 0) {
-                      return "Email cannot be empty";
+                      validForm = false;
+                      return "Name cannot be empty";
+                    } else if (val.indexOf(" ") != 1) {
+                      validForm = false;
+                      return "Please provide full name";
                     } else {
+                      validForm = true;
                       return null;
                     }
                   },
@@ -51,9 +61,60 @@ class NewUserFullName extends StatelessWidget {
                     fontFamily: "Poppins",
                   ),
                 ),
-              ])),
-            ));
-      }),
+                        new GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Splash()),
+                            );
+                          },
+                          child: ClipOval(
+                            child: Container(
+                                color: Colors.blue,
+                                height: 60.0, // height of the button
+                                width: 60.0, // width of the button
+                                child: new Icon(Icons.arrow_back)),
+                          ),
+                        ),
+                        new GestureDetector(
+                          onTap: () {
+                            if (validForm) {
+                              List<String> nameList =
+                              splitName(_fullNameController.text);
+                              RegistrationService registrationService =
+                              new RegistrationService();
+                              registrationService
+                                  .addCustomer(new SignUpForm(
+                                  firstName: nameList[0],
+                                  lastName: nameList[1],
+                                  phone: phone))
+                                  .whenComplete(() =>
+                              {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Splash()),
+                                )
+                              }).catchError((Object error) => {
+                              Fluttertoast.showToast(msg:error);
+                              });
+                            }
+                          },
+                          child: ClipOval(
+                            child: Container(
+                                color: Colors.blue,
+                                height: 60.0, // height of the button
+                                width: 60.0, // width of the button
+                                child: new Icon(Icons.arrow_forward)),
+                          ),
+                        )
+                      ])),
+                ));
+          }),
     );
+  }
+
+  List<String> splitName(String fullName) {
+    return fullName.split(" ");
   }
 }
