f9a506d8528b55f09764b73cf31614523b256f88
Matthew Slauson <slauson.matthew@gmail.om>
2020-07-26 15:30:50 -0500

    update to not use futures

--- lib/service/firebaseAdminService.dart
+++ lib/service/firebaseAdminService.dart
@@ -1,15 +1,13 @@
-import 'dart:async';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:main/service/authenticationService.dart';
 
 class FirebaseAdminService {
-  Future<void> updatePhone(
-      FirebaseUser firebaseUser, String phone, BuildContext context) async {
+  void updatePhone(
+      FirebaseUser firebaseUser, String phone, BuildContext context) {
     AuthenticationService authenticationService = new AuthenticationService();
     AuthCredential authCredential =
-        await authenticationService.otpCredentials(phone, context);
-    await firebaseUser.updatePhoneNumberCredential(authCredential);
+        authenticationService.otpCredentials(phone, context);
+    firebaseUser.updatePhoneNumberCredential(authCredential);
   }
 }
