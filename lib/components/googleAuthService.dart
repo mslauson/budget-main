34997bdf1cf237d34c209db5c5abf46661e51024
Matthew Slauson <slauson.matthew@gmail.om>
2020-07-28 18:56:32 -0500

    register customer if they haven't registered

--- lib/components/googleAuthService.dart
+++ lib/components/googleAuthService.dart
@@ -2,10 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:main/models/iam/signUpForm.dart';
 import 'package:main/service/authenticationService.dart';
+import 'package:main/service/registrationService.dart';
 import 'package:main/ui/util/phoneNumberAlert.dart' as phoneNumberAlert;
 
 class GoogleAuthService {
+  RegistrationService _registrationService = new RegistrationService();
   AuthenticationService _authService = new AuthenticationService(true);
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -24,6 +27,8 @@ class GoogleAuthService {
         email: googleSignInAccount.email);
     String phone = await _getPhoneNumber(context);
     if (signInMethods.isEmpty) {
+      await _registrationService.addCustomer(
+          _buildSignUpForm(googleSignInAccount, phone));
       _authService.signInWithCredentials(credential, phone, context);
     } else {
       _authService.signInWithCredentials(credential, null, context);
@@ -33,4 +38,22 @@ class GoogleAuthService {
   Future<String> _getPhoneNumber(BuildContext context) async {
     return phoneNumberAlert.PhoneNumberAlert.getPhoneNumber(context);
   }
+
+  SignUpForm _buildSignUpForm(GoogleSignInAccount googleSignInAccount,
+      String phoneNumber) {
+    if (googleSignInAccount.displayName.indexOf(" ") > 0) {
+      List<String> names = googleSignInAccount.displayName.split(" ");
+      return new SignUpForm(
+          firstName: names[0],
+          lastName: names[1],
+          emailAddress: googleSignInAccount.email,
+          phone: phoneNumber);
+    } else {
+      return new SignUpForm(
+          firstName: googleSignInAccount.displayName,
+          emailAddress: googleSignInAccount.email,
+          phone: phoneNumber);
+    }
+  }
+
 }
