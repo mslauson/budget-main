564d4c36c3ae8a1d8e2abc9806b2eab3a992ebeb
Matthew Slauson <slauson.matthew@gmail.om>
2020-07-31 16:50:15 -0500

    clean up/add comments

--- lib/constants/iamConstants.dart
+++ lib/constants/iamConstants.dart
@@ -3,45 +3,32 @@ class IAMConstants {
   //form Titles
   static const FORM_TITLE = "Sign Up";
   static const LOGIN_TITLE = "Login To Blossom";
+  static const FINAL_COLLECTION_TITLE =
+      "Please provide Blossom with some additional information";
 
   //Form Buttons
   static const SUBMIT = "Submit";
   static const CANCEL = "Cancel";
 
   //label text
-  static const String USERNAME = "Username";
-  static const String FIRST_NAME = "First Name";
-  static const String MIDDLE_INITIAL = "Middle Initial";
-  static const String LAST_NAME = "Last Name";
   static const String PHONE = "Phone Number";
-  static const String EMAIL_ADDRESS = "Email Address";
-  static const String PASSWORD = "Password";
+  static const String FULL_NAME = "Full Name";
+  static const String EMAIL = "Email";
 
   //hint text
-  static const String USERNAME_HINT = "Please choose a username";
-  static const String FIRST_NAME_HINT = "Please enter your first name";
-  static const String MIDDLE_INITIAL_HINT =
-      "Please enter your middle initial (Optional)";
-  static const String LAST_NAME_HINT = "Please enter your last name";
+  static const String FULL_NAME_HINT = "Please enter your full name";
   static const String PHONE_HINT =
       "Please Provide Blossom With\nYour Phone Number";
   static const String EMAIL_ADDRESS_HINT = "Please enter your email address";
   static const String PASSWORD_HINT = "Please choose a password";
 
   //Invalid Messages
-  static const String INVALID_USERNAME = "Username is required";
-  static const String INVALID_USERNAME_LENGTH =
-      "Username must be between 5-10 characters";
   static const String INVALID_EMAIL = "Invalid Email Address";
   static const String INVALID_EMAIL_REQUIRED = "Email Address is required";
   static const String INVALID_EMAIL_LENGTH =
       "Email address must be under 50 characters";
-  static const String INVALID_FIRST_NAME = "Invalid First Name";
-  static const String INVALID_MIDDLE_INITIAL = "Invalid Middle Initial";
-  static const String INVALID_LAST_NAME = "Invalid Last Name";
   static const String INVALID_PHONE = "Phone is required";
-  static const String INVALID_PASSWORD = "Password is required";
-  static const String USERNAME_TAKEN = "This username is Taken";
+  static const String INVALID_FULL_NAME = "Full name is required";
 
   //Success Message
   static const String REGISTRATION_SUCCESS =
