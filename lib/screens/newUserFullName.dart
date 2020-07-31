3dd89f6da8f2d8a73df7f4cc6d219436531f97a4
Matthew Slauson <slauson.matthew@gmail.om>
2020-07-31 17:04:49 -0500

    update screen for collecting name and email

--- lib/screens/newUserFullName.dart
+++ lib/screens/newUserFullName.dart
@@ -1,124 +1,114 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:main/models/global/activeUser.dart';
-import 'package:main/models/iam/signUpForm.dart';
-import 'package:main/screens/splash.dart';
-import 'package:main/service/auth/registrationService.dart';
-import 'package:main/ui/secureHome/secureHome.dart';
-import 'package:main/util/formUtils.dart';
-import 'package:scoped_model/scoped_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:main/components/iconActionButton.dart';
+import 'package:main/constants/iamConstants.dart';
+import 'package:main/theme/blossomText.dart';
+import 'package:main/ui/authenticate/authenticate.dart';
 
 class NewUserFullName extends StatelessWidget {
   final String phone;
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
   NewUserFullName({this.phone});
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController _fullNameController = new TextEditingController();
-    final formKey = GlobalKey<FormState>();
     return Scaffold(
-      body: ScopedModelDescendant<ActiveUser>(
-          builder: (BuildContext context, Widget child, ActiveUser model) {
-        return new Container(
-            padding: const EdgeInsets.all(30.0),
-            color: Colors.white,
-            child: new Container(
-              child: new Center(
-                  child: Form(
-                      key: formKey,
-                      child: new Column(children: [
-                        new Padding(padding: EdgeInsets.only(top: 140.0)),
-                        new Text(
-                          'Please Provide Blossom With Your Name',
-                          style:
-                              new TextStyle(color: Colors.blue, fontSize: 25.0),
-                        ),
-                        new Padding(padding: EdgeInsets.only(top: 50.0)),
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: "Enter Full Name",
-                            fillColor: Colors.white,
-                            border: new OutlineInputBorder(
-                              borderRadius: new BorderRadius.circular(25.0),
-                              borderSide: new BorderSide(),
-                            ),
-                          ),
-                          controller: _fullNameController,
-                          validator: (val) {
-                            int temp = val.trim().indexOf(" ");
-                            if (val.length == 0) {
-                              return "Name cannot be empty";
-                            }
-//                            else if (val.trim().indexOf(" ") != 1) {
-//                              return "Please provide full name";
-//                            }
-                            else {
-                              return null;
-                            }
-                          },
-                          keyboardType: TextInputType.text,
-                          style: new TextStyle(
-                            fontFamily: "Poppins",
-                          ),
-                        ),
-                        new GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => Splash()),
-                            );
-                          },
-                          child: ClipOval(
-                            child: Container(
-                                color: Colors.blue,
-                                height: 60.0, // height of the button
-                                width: 60.0, // width of the button
-                                child: new Icon(Icons.arrow_back)),
-                          ),
-                        ),
-                        new GestureDetector(
-                          onTap: () {
-                            if (FormUtils.validateCurrentForm(formKey)) {
-                              List<String> nameList =
-                              splitName(_fullNameController.text);
-                              RegistrationService registrationService =
-                              new RegistrationService();
-                              registrationService
-                                  .addCustomer(new SignUpForm(
-                                  firstName: nameList[0],
-                                  lastName: nameList[1],
-                                  phone: phone))
-                                  .whenComplete(() =>
-                              {
-                                Navigator.push(
-                                  context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  SecureHome()),
-                                        )
-                              })
-                                  .catchError((Object error) =>
-                              {Fluttertoast.showToast(msg: error)});
-                            }
-                          },
-                          child: ClipOval(
-                            child: Container(
-                                color: Colors.blue,
-                                height: 60.0, // height of the button
-                                width: 60.0, // width of the button
-                                child: new Icon(Icons.arrow_forward)),
-                          ),
-                        )
-                          ]))),
-                ));
-          }),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 50),
+              Text(IAMConstants.OTP_SCREEN_HEADER, style: BlossomText.title),
+              const SizedBox(height: 25),
+              TextFormField(
+                  decoration: new InputDecoration(
+                    labelText: IAMConstants.OTP_TXT_DECORATION,
+                    fillColor: Colors.white,
+                    border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.0),
+                      borderSide: new BorderSide(),
+                    ),
+                  ),
+                  controller: _fullNameController,
+                  validator: (val) => _validateName(val),
+                  keyboardType: TextInputType.text,
+                  style: BlossomText.body),
+              const SizedBox(height: 25),
+              TextFormField(
+                  decoration: new InputDecoration(
+                    labelText: IAMConstants.OTP_TXT_DECORATION,
+                    fillColor: Colors.white,
+                    border: new OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(25.0),
+                      borderSide: new BorderSide(),
+                    ),
+                  ),
+                  controller: _emailController,
+                  validator: (val) => _validateEmail(val),
+                  keyboardType: TextInputType.emailAddress,
+                  style: BlossomText.body),
+              const SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _buildButtons(context),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   List<String> splitName(String fullName) {
     return fullName.split(" ");
+  }
+
+  _buildButtons(BuildContext context) {
+    return <Widget>[
+      IconActionButton(
+          iconData: FontAwesomeIcons.arrowLeft,
+          onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Authenticate()),
+              )),
+      Padding(
+        padding: EdgeInsets.fromLTRB(45, 0, 45, 0),
+      ),
+      IconActionButton(
+          iconData: FontAwesomeIcons.arrowRight,
+          onPressed: () => {
+//            onSubmit(_controller.text.trim())
+              })
+    ];
+  }
+
+  String _validateName(String val) {
+    int temp = val.trim().indexOf(" ");
+    if (val.length == 0) {
+      return IAMConstants.INVALID_FULL_NAME;
+    }
+//                            else if (val.trim().indexOf(" ") != 1) {
+//                              return "Please provide full name";
+//                            }
+    else {
+      return null;
+    }
+  }
+
+  String _validateEmail(String val) {
+    if (val.length == 0) {
+      return IAMConstants.INVALID_EMAIL_REQUIRED;
+    } else if (!EmailValidator.validate(val)) {
+      return IAMConstants.INVALID_EMAIL;
+    } else {
+      return null;
+    }
   }
 }
