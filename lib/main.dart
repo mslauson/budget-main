82e41cc86bfcf626ed326eaa2167d7949b111868
Devlin Verley <verley93@gmail.com>
2020-08-01 16:03:23 -0500

    Force vertical orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:main/models/global/activeUser.dart';
 import 'package:main/screens/splash.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return ScopedModel<ActiveUser>(
         model: new ActiveUser(),
         child: MaterialApp(
@@ -23,8 +28,8 @@ class MyApp extends StatelessWidget {
                 bottomAppBarTheme: BottomAppBarTheme(color: Colors.lightBlue),
                 textTheme: TextTheme(
                     button: TextStyle(
-                  color: Colors.white,
-                ))),
+                      color: Colors.white,
+                    ))),
             home: new Splash()));
   }
 }
