44709167d6856eacb7043bda492517fb951c8369
Matthew Slauson <slauson.matthew@gmail.om>
2020-08-05 17:08:42 -0500

    create data access exception

--- lib/error/errorHandler.dart
+++ lib/error/errorHandler.dart
@@ -1,11 +1,17 @@
 import 'dart:developer';
 
 import 'package:http/http.dart';
+import 'package:main/error/data_access_exception.dart';
 
 class ErrorHandler{
   static onError(Response response, String context){
-    log(context + "failed with status code: "+response.statusCode.toString()+" response: "+response.body+"");
-    throw context + " failed.";
+    log(context +
+        "failed with status code: " +
+        response.statusCode.toString() +
+        " response: " +
+        response.body +
+        "");
+    throw DataAccessException(message: context + " failed.");
   }
 
 }
