d60b4d5d4a5886fdaa8caed526f5bdaec824c19c
Matthew Slauson <slauson.matthew@gmail.om>
2020-08-10 19:31:53 -0500

    add method for getting link token

--- lib/client/plaid_client.dart
+++ lib/client/plaid_client.dart
@@ -6,7 +6,9 @@ import 'package:main/constants/error_constants.dart';
 import 'package:main/constants/global_constants.dart';
 import 'package:main/constants/plaid_constants.dart';
 import 'package:main/error/error_handler.dart';
+import 'package:main/models/plaid/request/link_token_request.dart';
 import 'package:main/models/plaid/request/plaid_token_exchange_request.dart';
+import 'package:main/models/plaid/response/link_token_response.dart';
 import 'package:main/models/plaid/response/plaid_token_exchange_response.dart';
 import 'package:main/util/uri_builder.dart';
 
@@ -24,6 +26,20 @@ class PlaidClient {
     }
     PlaidTokenExchangeResponse plaidResponse =
         PlaidTokenExchangeResponse.fromJson(jsonDecode(response.body));
+    log(plaidResponse.toString());
+    return plaidResponse;
+  }
+
+  Future<LinkTokenResponse> getLinkToken(LinkTokenRequest request) async {
+    Response response = await post(
+        UriBuilder.plaidApiDev(PlaidConstants.URI_LINK_TOKEN),
+        headers: GlobalConstants.BASIC_POST_HEADERS,
+        body: request.toJson());
+    if (response.statusCode != 200) {
+      ErrorHandler.onError(response, ErrorConstants.ADDING_ACCOUNTS);
+    }
+    LinkTokenResponse plaidResponse =
+        LinkTokenResponse.fromJson(jsonDecode(response.body));
     log(plaidResponse.toString());
     return plaidResponse;
   }
