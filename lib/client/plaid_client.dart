f8e83c6f51fe1919690b56469eed896e6062a1d7
Matthew Slauson <slauson.matthew@gmail.om>
2020-08-15 14:21:56 -0500

    add client method for getting accounts

--- lib/client/plaid_client.dart
+++ lib/client/plaid_client.dart
@@ -7,9 +7,11 @@ import 'package:main/constants/global_constants.dart';
 import 'package:main/constants/plaid_constants.dart';
 import 'package:main/error/error_handler.dart';
 import 'package:main/models/plaid/request/link_token_request.dart';
+import 'package:main/models/plaid/request/plaid_accounts_request.dart';
 import 'package:main/models/plaid/request/plaid_institution_meta_request.dart';
 import 'package:main/models/plaid/request/plaid_token_exchange_request.dart';
 import 'package:main/models/plaid/response/link_token_response.dart';
+import 'package:main/models/plaid/response/plaid_accounts_response.dart';
 import 'package:main/models/plaid/response/plaid_institution_meta_response.dart';
 import 'package:main/models/plaid/response/plaid_token_exchange_response.dart';
 import 'package:main/util/uri_builder.dart';
@@ -56,6 +58,22 @@ class PlaidClient {
     }
     PlaidInstitutionMetaResponse metaResponse =
         PlaidInstitutionMetaResponse.fromJson(jsonDecode(response.body));
+    log(metaResponse.toString());
     return metaResponse;
+  }
+
+  Future<PlaidAccountsResponse> getAccounts(
+      PlaidAccountsRequest request) async {
+    Response response = await post(
+        UriBuilder.plaidApiDev(PlaidConstants.URI_GET_ACCOUNTS),
+        headers: GlobalConstants.BASIC_POST_HEADERS,
+        body: jsonEncode(request.toJson()));
+    if (response.statusCode != 200) {
+      ErrorHandler.onErrorClient(response, ErrorConstants.ADDING_ACCOUNTS);
+    }
+    PlaidAccountsResponse accountsResponse =
+        PlaidAccountsResponse.fromJson(jsonDecode(response.body));
+    log(accountsResponse.toString());
+    return accountsResponse;
   }
 }
