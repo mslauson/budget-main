7770e5294af2b1d1a565608d6c8599507fdea7e8
mslauson <slauson.matthew@gmail.com>
2020-08-30 14:03:23 -0500

    finish getting accounts with initialization service

--- lib/service/secure/home_initialization_service.dart
+++ lib/service/secure/home_initialization_service.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:scoped_model/scoped_model.dart';
 import 'package:main/models/accounts/accounts_full_model.dart';
 import 'package:main/service/accounts/accounts_service.dart';
 
@@ -11,18 +10,14 @@ class HomeInitializationService {
   final Function() getExpenses;
   final Function() getCustomer;
 
-  HomeInitializationService(this.getTransactions, this.getBudgets,
-      this.getExpenses, this.getCustomer, {this.getAccounts});
+  HomeInitializationService(
+      this.getTransactions, this.getBudgets, this.getExpenses, this.getCustomer,
+      {this.getAccounts});
 
   Future<void> loadData(String phone, BuildContext context) async {
-    ScopedModel.of<AccountsFullModel>(context, rebuildOnChange: true)
-    .
-    await
-    _accountsService
-    .
-    getAccountsForUser
-    (
-    phone
-    );
+    getAccounts(await getAccountsByPhone(phone));
   }
+
+  Future<AccountsFullModel> getAccountsByPhone(String phone) async =>
+      await _accountsService.getAccountsForUser(phone);
 }
