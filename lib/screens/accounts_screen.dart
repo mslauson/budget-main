7dd6995d1d2bda4a0bf727877e8f6be1e54adde1
mslauson <slauson.matthew@gmail.com>
2020-09-01 18:59:24 -0500

    build card with institution logo and name

--- lib/screens/accounts_screen.dart
+++ lib/screens/accounts_screen.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:main/components/drawer_container.dart';
+import 'package:main/models/accounts/response/accounts_response.dart';
+import 'package:main/models/global/activeUser.dart';
+import 'package:main/service/secure/home_initialization_service.dart';
 import 'package:main/theme/blossom_text.dart';
 import 'package:main/widgets/nav_drawer.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class AccountsScreen extends StatefulWidget {
   @override
@@ -10,8 +16,11 @@ class AccountsScreen extends StatefulWidget {
 }
 
 class _AccountsScreenState extends State<AccountsScreen> {
+  AccountsResponseModel accountsResponseModel;
+
   @override
   Widget build(BuildContext context) {
+    _loadAccounts();
     return Scaffold(
       body: Stack(
         children: [
@@ -24,5 +33,42 @@ class _AccountsScreenState extends State<AccountsScreen> {
         ],
       ),
     );
+  }
+
+  _loadAccounts() {
+    final String phone =
+        ScopedModel.of<ActiveUser>(context, rebuildOnChange: true).phone;
+    final HomeInitializationService initializationService =
+        HomeInitializationService(
+            getAccounts: (AccountsResponseModel fullModel) {
+      this.accountsResponseModel = fullModel;
+    });
+    initializationService.loadData(phone, context);
+  }
+
+  List<Widget> _buildAccountsByType(
+      AccountsResponseModel accountsResponseModel) {}
+
+  List<Widget> _buildAccountsByInstitution() {
+    List<Widget> accountsWidgets = new List();
+    accountsResponseModel.itemList.forEach((accountsModel) {
+      accountsWidgets.add(
+        Card(
+            child: Column(
+          children: [
+            ListTile(
+              leading: Image.memory(
+                base64Decode(accountsModel.institution.logo),
+                height: 10,
+                width: 10,
+              ),
+              title: Text(accountsModel.institution.name,
+                  style: BlossomText.title),
+            )
+          ],
+        )),
+      );
+    });
+    return accountsWidgets;
   }
 }
