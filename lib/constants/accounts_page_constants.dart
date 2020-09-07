c0c3c0ec34bfad0ed6e86120a618378e5cbac836
Matthew Slauson <slauson.matthew@gmail.om>
2020-09-07 12:59:34 -0500

    add constants for handling plaid accounts

--- lib/constants/accounts_page_constants.dart
+++ lib/constants/accounts_page_constants.dart
@@ -1,3 +1,16 @@
 class AccountsPageConstants{
   static const String ACCOUNTS = "Accounts";
+  static const String DEPOSITORY_TYPE = "depository";
+  static const String CREDIT_TYPE = "credit";
+  static const String INVESTMENT_TYPE = "investment";
+  static const String LOAN_TYPE = "loan";
+  static const String CHECKING_SUB_TYPE = "checking";
+  static const String SAVINGS_SUB_TYPE = "savings";
+  static const String CD_SUB_TYPE = "cd";
+  static const String CREDIT_CARD_SUB_TYPE = "credit card";
+  static const String MONEY_MARKET_SUB_TYPE = "money market";
+  static const String IRA_SUB_TYPE = "ira";
+  static const String FOUR_O_ONE_K_SUB_TYPE = "401k";
+  static const String STUDENT_SUB_TYPE = "student";
+  static const String MORTGAGE_SUB_TYPE = "mortgage";
 }
