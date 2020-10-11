9c9a4f9a4f5f5e3e0bc4f603b58c68c7215a9866
mslauson <slauson.matthew@gmail.com>
2020-10-11 16:44:55 -0500

    add file for neumorphic styles

---
+++ lib/theme/blossom_neumorphic_styles.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class BlossomNeumorphicStyles {
+  BlossomNeumorphicStyles._();
+
+  static final four = NeumorphicStyle(
+    depth: 4, //customize depth here
+    color: Colors.black, //customize color here
+  );
+
+  static final fourWhite = NeumorphicStyle(
+    depth: 4, //customize depth here
+    color: Colors.white, //customize color here
+  );
+
+  static final three = NeumorphicStyle(
+    depth: 3, //customize depth here
+    color: Colors.black, //customize color here
+  );
+
+  static final threeWhite = NeumorphicStyle(
+    depth: 3, //customize depth here
+    color: Colors.white, //customize color here
+  );
+
+  static final two = NeumorphicStyle(
+    depth: 2, //customize depth here
+    color: Colors.black, //customize color here
+  );
+
+  static final twoWhite = NeumorphicStyle(
+    depth: 2, //customize depth here
+    color: Colors.white, //customize color here
+  );
+
+  static final one = NeumorphicStyle(
+    depth: 1, //customize depth here
+    color: Colors.black, //customize color here
+  );
+
+  static final oneWhite = NeumorphicStyle(
+    depth: 1, //customize depth here
+    color: Colors.white, //customize color here
+  );
+}
