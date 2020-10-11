9bfda968f9c59ac36e39f3a852b83df55856ee93
mslauson <slauson.matthew@gmail.com>
2020-10-11 07:32:47 -0500

    add neumorphic font file

---
+++ lib/theme/blossom_neumorphic_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+/// With the Neumorphic Framework, we have to specify the color in the text
+/// widget itself
+class BlossomText {
+  BlossomText._();
+
+  static final headline = NeumorphicTextStyle(
+      fontSize: 36, fontWeight: FontWeight.w700, fontFamily: 'Neuton');
+
+  static final title = NeumorphicTextStyle(
+      fontSize: 32, fontWeight: FontWeight.w500, fontFamily: 'Mulish');
+
+  static final largeBody = NeumorphicTextStyle(
+      fontSize: 22, fontWeight: FontWeight.w300, fontFamily: 'Imprima');
+
+  static final body = NeumorphicTextStyle(
+      fontSize: 18, fontWeight: FontWeight.w300, fontFamily: 'Imprima');
+
+  static final mediumBody = NeumorphicTextStyle(
+      fontSize: 16, fontWeight: FontWeight.w300, fontFamily: 'Imprima');
+
+  static final secondaryBody = NeumorphicTextStyle(
+      fontSize: 14, fontWeight: FontWeight.w300, fontFamily: 'Imprima');
+
+  static final accountNumber = NeumorphicTextStyle(
+      fontSize: 14, fontWeight: FontWeight.w900, fontFamily: 'Orbitron');
+}
