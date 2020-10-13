1cbd3c65b9c04f591d0ee5dbec46644d3645dcc6
mslauson <slauson.matthew@gmail.com>
2020-10-13 18:11:44 -0500

    start icon constants

---
+++ lib/constants/budget_icons_enum.dart
@@ -0,0 +1,1 @@
+enum BudgetIconsEnum { Travel, Food, Payment, Shops, Transfer, Recreation }
---
+++ lib/theme/budget_icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BudgetIcons {
+  BudgetIcons._();
+
+  static const food = Icon(FontAwesomeIcons.utensils);
+  static const transportation = Icon(FontAwesomeIcons.car);
+  static const shopping = Icon(FontAwesomeIcons.shoppingBag);
+  static const deposit = Icon(FontAwesomeIcons.dollarSign);
+  static const withdraw = Icon(FontAwesomeIcons.university);
+  static const payment = Icon(FontAwesomeIcons.moneyCheck);
+  static const health = Icon(FontAwesomeIcons.starOfLife);
+}
