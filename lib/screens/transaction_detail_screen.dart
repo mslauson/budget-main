900bddb085bfc23befabe86f0d6767d73a4fcab7
mslauson <slauson.matthew@gmail.com>
2020-11-05 13:23:17 -0600

    display title

--- lib/screens/transaction_detail_screen.dart
+++ lib/screens/transaction_detail_screen.dart
@@ -1,16 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-import 'package:main/components/drawer_container.dart';
-import 'package:main/constants/accounts_page_constants.dart';
-import 'package:main/constants/transaction_page_constants.dart';
-import 'package:main/models/accounts/account.dart';
 import 'package:main/models/transactions/transactions.dart';
-import 'package:main/theme/blossom_text.dart';
-import 'package:main/util/parse_utils.dart';
-import 'package:main/widgets/nav_drawer.dart';
+import 'package:main/theme/blossom_neumorphic_styles.dart';
+import 'package:main/theme/blossom_neumorphic_text.dart';
 
 class AccountDetailScreen extends StatelessWidget {
   final Transactions _transaction;
@@ -21,43 +14,19 @@ class AccountDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        NavDrawer(),
-        DrawerContainer(children: [
-          Column(
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  NeumorphicIcon(_icon.icon),
-                  Text(
-                    _transaction.merchant,
-                    style: BlossomText.title,
-                  )
-                ],
+              NeumorphicIcon(_icon.icon),
+              NeumorphicText(_transaction.merchant,
+                  textStyle: BlossomNeumorphicText.title,
+                  style: BlossomNeumorphicStyles.four)
+            ],
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Card(
-                    margin: EdgeInsets.all(20),
-                    child: ListTile(
-                        title: Text(AccountsPageConstants.AVAILABLE_BALANCE,
-                            style: BlossomText.body),
-                        subtitle: ParseUtils.parseAvailableBalance(
-                            _account.balances.current))),
-              ),
-              Padding(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Card(
-                    margin: EdgeInsets.all(20),
-                    child: ListTile(
-                        title: Text(TransactionsPageConstants.TRANSACTIONS,
-                            style: BlossomText.body)),
-                  ))
             ],
           ),
-        ])
-      ]),
     );
   }
 }
