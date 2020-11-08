dfddf1834500e194b0d03f6c634902ced2e811ce
matthewslauson <slauson.matthew@gmail.com>
2020-11-08 08:12:38 -0600

    add title

--- lib/screens/budgets_screen.dart
+++ lib/screens/budgets_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:loading/indicator/ball_pulse_indicator.dart';
 import 'package:loading/loading.dart';
 import 'package:main/client/budget_client.dart';
 import 'package:main/components/drawer_container.dart';
+import 'package:main/models/budget/getBudgetsResponse.dart';
 import 'package:main/models/global/activeUser.dart';
+import 'package:main/theme/blossom_neumorphic_styles.dart';
+import 'package:main/theme/blossom_neumorphic_text.dart';
 import 'package:main/theme/blossom_text.dart';
 import 'package:main/util/date_utils.dart';
 import 'package:main/widgets/nav_drawer.dart';
@@ -45,6 +49,17 @@ class _BudgetsScreenState extends State<BudgetsScreen> {
   Future<List<Widget>> _loadBudgets() async{
     final String phone =
         ScopedModel.of<ActiveUser>(context, rebuildOnChange: true).phone;
-    await _budgetClient.getBudgetsForUser(phone, DateUtils.currentFirstOfMonthIso());
+    GetBudgetsResponse budgetResponse = await _budgetClient.getBudgetsForUser(phone, DateUtils.currentFirstOfMonthIso());
+    return await _buidBudgets(budgetResponse);
   }
+
+  Future<List<Widget>> _buidBudgets(GetBudgetsResponse budgetResponse) async{
+    List<Widget> _budgetWidgets = new List();
+    _budgetWidgets.add(NeumorphicText(
+        'Budgets',
+        textStyle: BlossomNeumorphicText.headline,
+        style: BlossomNeumorphicStyles.eightGrey));
+    return _budgetWidgets;
+  }
+
 }
