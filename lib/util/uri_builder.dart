426fc02c53dc97b9deaf9231f46b0aea49143b28
matthewslauson <slauson.matthew@gmail.com>
2020-11-08 07:04:58 -0600

    fix budget client

--- lib/util/uri_builder.dart
+++ lib/util/uri_builder.dart
@@ -9,6 +9,14 @@ class UriBuilder {
     return 'http://dev.blossombudgeting.io/$service/api/v$version/$pathParam/';
   }
 
+  static blossomDevWithPathAndUri(String service, int version, String pathParam, String uri) {
+    return 'http://dev.blossombudgeting.io/$service/api/v$version/$pathParam/$uri';
+  }
+
+  static blossomDevWithTwoPathAndUri(String service, int version, String pathParam1,String pathParam2, String uri) {
+    return 'http://dev.blossombudgeting.io/$service/api/v$version/$pathParam1/$uri/$pathParam2';
+  }
+
   @Deprecated("using native sdk now")
   static plaidDev(String publicKey, String env, String products) {
     return 'https://cdn.plaid.com/link/v2/stable/link.html?isWebview=true'
