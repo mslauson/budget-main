6d45aa89b4b7b48f718ba9fad2f1345dfc36cc1e
matthewslauson <slauson.matthew@gmail.com>
2020-11-23 14:27:53 -0600

    update to not return zero

--- lib/util/math_utils.dart
+++ lib/util/math_utils.dart
@@ -3,6 +3,9 @@ class MathUtils {
 
   static String getAvailabileBalance(double allocated, double used) {
     double available = allocated - used;
+    if (available.isNegative) {
+      available = 0;
+    }
     return "\$" + available.toStringAsFixed(2);
   }
 }
