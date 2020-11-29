20ba0556b47daf0f6ec13f90232854045f319840
matthewslauson <slauson.matthew@gmail.com>
2020-11-29 17:05:13 -0500

    make transaction detail screen open when a transaction is opened on the budget screen

--- lib/screens/budgets_screen.dart
+++ lib/screens/budgets_screen.dart
@@ -6,14 +6,18 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:loading/indicator/ball_pulse_indicator.dart';
 import 'package:loading/loading.dart';
+import 'package:main/client/accounts_client.dart';
 import 'package:main/client/budget_client.dart';
 import 'package:main/client/transactions_client.dart';
 import 'package:main/components/drawer_container.dart';
 import 'package:main/constants/budget_screen_constants.dart';
 import 'package:main/constants/transaction_microservice_constants.dart';
+import 'package:main/models/accounts/account_meta.dart';
+import 'package:main/models/accounts/response/account_meta_response.dart';
 import 'package:main/models/budget/getBudgetsResponse.dart';
 import 'package:main/models/global/activeUser.dart';
 import 'package:main/models/transactions/transactions_get_response.dart';
+import 'package:main/screens/transaction_detail_screen.dart';
 import 'package:main/theme/blossom_neumorphic_styles.dart';
 import 'package:main/theme/blossom_neumorphic_text.dart';
 import 'package:main/util/date_utils.dart';
@@ -30,6 +34,8 @@ class BudgetsScreen extends StatefulWidget {
 class _BudgetsScreenState extends State<BudgetsScreen> {
   final BudgetClient _budgetClient = BudgetClient();
   final TransactionsClient _transactionsClient = TransactionsClient();
+  final AccountsClient _accountsClient = AccountsClient();
+  AccountMetaResponse _metaResponse;
 
   @override
   Widget build(BuildContext context) {
@@ -78,13 +84,13 @@ class _BudgetsScreenState extends State<BudgetsScreen> {
     return _budgetWidgets;
   }
 
-  Future<List<Widget>> _buildWidgetForBudgets(GetBudgetsResponse budgetResponse,
-      String phone) async {
+  Future<List<Widget>> _buildWidgetForBudgets(
+      GetBudgetsResponse budgetResponse, String phone) async {
     List<Widget> widgets = new List();
     TransactionsGetResponse response = await _getTransactionsForBudgets(phone);
     budgetResponse.budgets.forEach((budget) {
-      List<Transactions> transactions = _filterTransactionsForBudget(
-          response, budget.id);
+      List<Transactions> transactions =
+          _filterTransactionsForBudget(response, budget.id);
       List<Transactions> transactionsSubSet = transactions.sublist(0, 3);
       widgets.add(Padding(
         padding: const EdgeInsets.all(8.0),
@@ -185,6 +191,7 @@ class _BudgetsScreenState extends State<BudgetsScreen> {
 
   Future<TransactionsGetResponse> _getTransactionsForBudgets(
       String phone) async {
+    _metaResponse = await _accountsClient.getAccountMetaDataForUser(phone);
     return await _transactionsClient.getTransactionsForUser(
         phone,
         TransactionsMicroserviceConstants.DATE_TIME_RANGE_QUERY,
@@ -194,8 +201,9 @@ class _BudgetsScreenState extends State<BudgetsScreen> {
 
   List<Transactions> _filterTransactionsForBudget(
       TransactionsGetResponse response, String budgetId) {
-    return response.transactions.where((element) =>
-    element.budgetId == budgetId).toList();
+    return response.transactions
+        .where((element) => element.budgetId == budgetId)
+        .toList();
   }
 
   List<Widget> _buildTransactionWidgets(List<Transactions> transactionList) {
@@ -203,35 +211,45 @@ class _BudgetsScreenState extends State<BudgetsScreen> {
     transactionWidgets.add(Divider());
     int i = 0;
     transactionList.forEach((transaction) {
+      AccountMeta _currentMeta =
+      ParseUtils.getCorrectMeta(_metaResponse, transaction.accountId);
+      Icon iconData = ParseUtils.getIconForTransaction(transaction);
       transactionWidgets.add(Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            NeumorphicText(transaction.merchant,
-                textStyle: BlossomNeumorphicText.mediumBody,
-                style: BlossomNeumorphicStyles.fourGrey),
-            Spacer(flex: 2),
-            Neumorphic(
-                  style: BlossomNeumorphicStyles.eightConcave,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
-                    child: NeumorphicText(
-                      ParseUtils.checkIfNegative(
-                          ParseUtils.formatAmount(
-                              transaction.amount)),
-                      textStyle: BlossomNeumorphicText.mediumBody,
-                      style: BlossomNeumorphicStyles.fourGrey,
-                    ),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      TransactionDetailScreen(
+                          transaction, _currentMeta, iconData)),
+            );
+          },
+          child: Row(
+            children: [
+              NeumorphicText(transaction.merchant,
+                  textStyle: BlossomNeumorphicText.mediumBody,
+                  style: BlossomNeumorphicStyles.fourGrey),
+              Spacer(flex: 2),
+              Neumorphic(
+                style: BlossomNeumorphicStyles.eightConcave,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                  child: NeumorphicText(
+                    ParseUtils.checkIfNegative(
+                        ParseUtils.formatAmount(transaction.amount)),
+                    textStyle: BlossomNeumorphicText.mediumBody,
+                    style: BlossomNeumorphicStyles.fourGrey,
                   ),
                 ),
-              ],
-            ),
-          )
-      );
+              ),
+            ],
+          ),
+        ),
+      ));
       if (i < transactionList.length - 1) {
-        transactionWidgets.add(
-            Divider()
-        );
+        transactionWidgets.add(Divider());
         i++;
       }
     });
