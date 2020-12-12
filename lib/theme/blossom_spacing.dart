8dba61ab47f557a89b295fdc4e0c1e547019eb5f
matthewslauson <slauson.matthew@gmail.com>
2020-12-12 08:24:57 -0600

    add file for spacing rules

---
+++ lib/theme/blossom_spacing.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class BlossomSpacing {
+  BlossomSpacing._();
+
+  static const Padding STANDARD_FORM =
+      Padding(padding: EdgeInsets.only(top: 15, bottom: 15));
+}
