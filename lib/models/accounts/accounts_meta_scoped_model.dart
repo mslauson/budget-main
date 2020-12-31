c46ef7b5d86a6f7faf82c594c8ffd4326dcd99e9
matthewslauson <slauson.matthew@gmail.com>
2020-12-31 08:50:39 -0600

    add scoped models for accounts

---
+++ lib/models/accounts/accounts_meta_scoped_model.dart
@@ -0,0 +1,6 @@
+import 'package:main/models/accounts/response/account_meta_response.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class AccountsMetaScopedModel extends Model {
+  AccountMetaResponse responseModel;
+}
---
+++ lib/models/accounts/accounts_sccoped_model.dart
@@ -0,0 +1,6 @@
+import 'package:main/models/accounts/response/accounts_response.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class AccountsScopedModel extends Model {
+  AccountsResponseModel responseModel;
+}
