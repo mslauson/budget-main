f1d40634b1f0ab9570f9829b4fd7f4d44636a508
matthewslauson <slauson.matthew@gmail.com>
2020-12-31 09:49:03 -0600

    populate transactions scoped model

--- lib/service/secure/home_initialization_service.dart
+++ lib/service/secure/home_initialization_service.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:main/models/accounts/accounts_sccoped_model.dart';
+import 'package:main/models/transactions/transactions_scoped_model.dart';
 import 'package:main/service/accounts/accounts_service.dart';
+import 'package:main/service/transactions/transactions_service.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class HomeInitializationService {
   final AccountsService _accountsService = AccountsService();
+  final TransactionsService _transactionsService = TransactionsService();
 
   HomeInitializationService();
 
@@ -15,4 +18,10 @@ class HomeInitializationService {
   Future<void> getAccountsByPhone(String phone, BuildContext context) async =>
       ScopedModel.of<AccountsScopedModel>(context, rebuildOnChange: true)
           .responseModel = await _accountsService.getAccountsForUser(phone);
+
+  Future<void> getTransactionsByPhone(
+          String phone, BuildContext context) async =>
+      ScopedModel.of<TransactionsScopedModel>(context, rebuildOnChange: true)
+              .responseModel =
+          await _transactionsService.getTransactionsByPhone(phone);
 }
--- lib/service/transactions/transactions_service.dart
+++ lib/service/transactions/transactions_service.dart
@@ -1,6 +1,9 @@
 import 'package:main/client/transactions_client.dart';
+import 'package:main/constants/transaction_microservice_constants.dart';
 import 'package:main/models/plaid/response/plaid_transactions_response.dart';
 import 'package:main/models/transactions/request/transactions_batch_request.dart';
+import 'package:main/models/transactions/transactions_get_response.dart';
+import 'package:main/util/date_utils.dart';
 
 class TransactionsService {
   final TransactionsClient _transactionsClient = TransactionsClient();
@@ -19,5 +22,13 @@ class TransactionsService {
     });
     return TransactionsBatchRequest(
         transactions: transactionsResponse.transactions);
+  }
+
+  Future<TransactionsGetResponse> getTransactionsByPhone(String phone) async {
+    return await _transactionsClient.getTransactionsForUser(
+        phone,
+        TransactionsMicroserviceConstants.DATE_TIME_RANGE_QUERY,
+        DateUtils.currentLastOfMonthIso(),
+        DateUtils.currentDateIso());
   }
 }
