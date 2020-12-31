aff03325d3adb1d55f8cf4096edbd7a97c521238
matthewslauson <slauson.matthew@gmail.com>
2020-12-31 10:10:25 -0600

    make methods private

--- lib/service/secure/home_initialization_service.dart
+++ lib/service/secure/home_initialization_service.dart
@@ -15,20 +15,22 @@ class HomeInitializationService {
   HomeInitializationService();
 
   Future<void> loadData(String phone, BuildContext context) async {
-    await getAccountsByPhone(phone, context);
+    await _getAccountsByPhone(phone, context);
+    await _getTransactionsByPhone(phone, context);
+    await _getBudgetsByPhone(phone, context);
   }
 
-  Future<void> getAccountsByPhone(String phone, BuildContext context) async =>
+  Future<void> _getAccountsByPhone(String phone, BuildContext context) async =>
       ScopedModel.of<AccountsScopedModel>(context, rebuildOnChange: true)
           .responseModel = await _accountsService.getAccountsForUser(phone);
 
-  Future<void> getTransactionsByPhone(
+  Future<void> _getTransactionsByPhone(
           String phone, BuildContext context) async =>
       ScopedModel.of<TransactionsScopedModel>(context, rebuildOnChange: true)
               .responseModel =
           await _transactionsService.getTransactionsByPhone(phone);
 
-  Future<void> getBudgetsByPhone(String phone, BuildContext context) async =>
+  Future<void> _getBudgetsByPhone(String phone, BuildContext context) async =>
       ScopedModel.of<BudgetsScopedModel>(context, rebuildOnChange: true)
           .responseModel = await _budgetsService.getBudgetsForUser(phone);
 }
