336f1432c6294c40ba3dec4f9e986f5b958ffb86
matthewslauson <slauson.matthew@gmail.com>
2020-12-31 09:21:32 -0600

    add more scoped models

---
+++ lib/models/budget/budgets_scoped_model.dart
@@ -0,0 +1,6 @@
+import 'package:main/models/budget/getBudgetsResponse.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class BudgetsScopedModel extends Model {
+  GetBudgetsResponse responseModel;
+}
---
+++ lib/models/transactions/transactions_scoped_model.dart
@@ -0,0 +1,6 @@
+import 'package:main/models/transactions/transactions_get_response.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class TransactionsScopedModel extends Model {
+  TransactionsGetResponse responseModel;
+}
