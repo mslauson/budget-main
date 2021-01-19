232d13c67d9f8ffcf6fc73269cc36ad95116e9fa
matthewslauson <slauson.matthew@gmail.com>
2021-01-19 15:43:31 -0600

    add test for encryption

--- test/encryption_test.dart
+++ test/encryption_test.dart
@@ -5,9 +5,10 @@ void main() {
   test("Test encryption", () async {
     final BlossomEncryptionUtility ec = BlossomEncryptionUtility();
     String testString = "This is a test for super cool encryption";
-    String encrypted = ec.encrypt(testString);
+    final encrypted = ec.encrypt(testString);
     print(encrypted);
     String decrypted = ec.decrypt(encrypted);
     print(decrypted);
+    expect(testString, decrypted);
   });
 }
