738b01ea2a499420593e0f52c08eebf5039d29d0
matthewslauson <slauson.matthew@gmail.com>
2021-01-19 20:45:09 -0600

    decrypt accounts response model

--- lib/util/model_encryption_utility.dart
+++ lib/util/model_encryption_utility.dart
@@ -1,3 +1,7 @@
+import 'package:main/models/accounts/account.dart';
+import 'package:main/models/accounts/accounts_full_model.dart';
+import 'package:main/models/accounts/institution.dart';
+import 'package:main/models/accounts/response/accounts_response.dart';
 import 'package:main/models/budget/getBudgetsResponse.dart';
 import 'package:main/models/iam/signUpForm.dart';
 import 'package:main/security/blossom_encryption_utility.dart';
@@ -36,6 +40,25 @@ class ModelEncryptionUtility {
     return GetBudgetsResponse(budgets: _decryptedBudgets);
   }
 
+  AccountsResponseModel decryptAccountsResponseModel(
+      AccountsResponseModel accountsResponseModel) {
+    List<AccountsFullModel> decryptedAccounts = List();
+    accountsResponseModel.itemList.forEach((item) {
+      decryptedAccounts.add(AccountsFullModel(
+          id: _beu.decrypt(item.id),
+          phone: _beu.decrypt(item.phone),
+          accessToken: _beu.decrypt(item.accessToken),
+          accounts: _decryptAccounts(item.accounts),
+          deletionTimeStamp: item.deletionTimeStamp,
+          flaggedForDeletion: item.flaggedForDeletion,
+          institution: _decryptInstitution(item.institution),
+          lastUpdated: item.lastUpdated,
+          linkSessionId: _beu.decrypt(item.linkSessionId),
+          needsUpdating: item.needsUpdating));
+    });
+    return AccountsResponseModel(itemList: decryptedAccounts);
+  }
+
   List<LinkedTransactions> _decryptLinkedTransactions(Budgets budget) {
     List<LinkedTransactions> _decryptedLinkedTransactions = List();
     budget.linkedTransactions.forEach((linkedTrans) {
@@ -46,8 +69,7 @@ class ModelEncryptionUtility {
     return _decryptedLinkedTransactions;
   }
 
-  List<LinkedTransactions> _decryptSubLinkedTransactions(
-      SubCategory subCategory) {
+  List<LinkedTransactions> _decryptSubLinkedTransactions(SubCategory subCategory) {
     List<LinkedTransactions> _decryptedLinkedTransactions = List();
     subCategory.linkedTransactions.forEach((linkedTrans) {
       _decryptedLinkedTransactions.add(LinkedTransactions(
@@ -70,5 +92,32 @@ class ModelEncryptionUtility {
           linkedTransactions: _decryptSubLinkedTransactions(subCat)));
     });
     return _decryptedSubCategory;
+  }
+
+  List<Account> _decryptAccounts(List<Account> accounts) {
+    List<Account> accountList = List();
+    accounts.forEach((account) {
+      accountList.add(Account(
+          id: _beu.decrypt(account.id),
+          name: _beu.decrypt(account.name),
+          accountId: _beu.decrypt(account.accountId),
+          balances: account.balances,
+          mask: _beu.decrypt(account.mask),
+          subtype: _beu.decrypt(account.subtype),
+          type: _beu.decrypt(account.type),
+          verificationStatus: _beu.decrypt(account.verificationStatus)));
+    });
+    return accountList;
+  }
+
+  Institution _decryptInstitution(Institution institution) {
+    return Institution(
+        type: _beu.decrypt(institution.type),
+        name: _beu.decrypt(institution.name),
+        institutionId: _beu.decrypt(institution.institutionId),
+        label: _beu.decrypt(institution.label),
+        logo: _beu.decrypt(institution.logo),
+        primaryColor: _beu.decrypt(institution.primaryColor),
+        url: _beu.decrypt(institution.url));
   }
 }
