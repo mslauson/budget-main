9c019feb68538ed1451331e505eeefbaa1884eb9
matthewslauson <slauson.matthew@gmail.com>
2021-01-24 15:50:25 -0600

    make more null safe

--- lib/service/secure/home_initialization_service.dart
+++ lib/service/secure/home_initialization_service.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:main/models/accounts/response/account_meta_response.dart';
+import 'package:main/models/accounts/response/accounts_response.dart';
+import 'package:main/models/budget/getBudgetsResponse.dart';
 import 'package:main/models/global/activeUser.dart';
+import 'package:main/models/transactions/transactions_get_response.dart';
 import 'package:main/service/accounts/accounts_service.dart';
 import 'package:main/service/budgets/budgets_service.dart';
 import 'package:main/service/transactions/transactions_service.dart';
@@ -19,20 +23,40 @@ class HomeInitializationService {
     await getBudgetsByPhone(phone, context);
   }
 
-  Future<void> getAccountsByPhone(String phone, BuildContext context) async =>
+  Future<void> getAccountsByPhone(String phone, BuildContext context) async {
+    AccountsResponseModel fullModel =
+        await _accountsService.getAccountsForUser(phone);
+    if (fullModel != null) {
       ScopedModel.of<ActiveUser>(context, rebuildOnChange: true).accounts =
-          await _accountsService.getAccountsForUser(phone);
+          fullModel;
+    }
+  }
 
-  Future<void> getMetaByPhone(String phone, BuildContext context) async =>
+  Future<void> getMetaByPhone(String phone, BuildContext context) async {
+    AccountMetaResponse metaResponse =
+        await _accountsService.getMetaForUser(phone);
+    if (metaResponse != null) {
       ScopedModel.of<ActiveUser>(context, rebuildOnChange: true).meta =
-          await _accountsService.getMetaForUser(phone);
+          metaResponse;
+    }
+  }
 
   Future<void> getTransactionsByPhone(
-          String phone, BuildContext context) async =>
+      String phone, BuildContext context) async {
+    TransactionsGetResponse transactionsGetResponse =
+        await _transactionsService.getTransactionsByPhone(phone);
+    if (transactionsGetResponse != null) {
       ScopedModel.of<ActiveUser>(context, rebuildOnChange: true).transactions =
-          await _transactionsService.getTransactionsByPhone(phone);
+          transactionsGetResponse;
+    }
+  }
 
-  Future<void> getBudgetsByPhone(String phone, BuildContext context) async =>
+  Future<void> getBudgetsByPhone(String phone, BuildContext context) async {
+    GetBudgetsResponse getBudgetsResponse =
+        await _budgetsService.getBudgetsForUser(phone);
+    if (getBudgetsResponse != null) {
       ScopedModel.of<ActiveUser>(context, rebuildOnChange: true).budgets =
-          await _budgetsService.getBudgetsForUser(phone);
+          getBudgetsResponse;
+    }
+  }
 }
