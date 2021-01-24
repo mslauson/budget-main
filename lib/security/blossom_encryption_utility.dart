a8ac74b2d9bdad74113516468a97d3bd55ec8169
matthewslauson <slauson.matthew@gmail.com>
2021-01-24 15:55:15 -0600

    make more null safe

--- lib/security/blossom_encryption_utility.dart
+++ lib/security/blossom_encryption_utility.dart
@@ -5,25 +5,31 @@ class BlossomEncryptionUtility {
   final String initVector = "CharlieCatIV!@#%";
 
   String encrypt(String plainText) {
-    final key = Key.fromUtf8(keyMain);
-    final iv = IV.fromUtf8(initVector);
-    final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
-    print("Dart Output…!!!");
-    print("IV: " + iv.bytes.toString());
-    print("Key: " + key.bytes.toString());
-    final encrypted = encrypter.encrypt(plainText, iv: iv);
-    print("Encrypted: " + encrypted.bytes.toString());
-    print("Base64: " + encrypted.base64);
-    return encrypted.base64;
+    if (plainText != null) {
+      final key = Key.fromUtf8(keyMain);
+      final iv = IV.fromUtf8(initVector);
+      final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
+      print("Dart Output…!!!");
+      print("IV: " + iv.bytes.toString());
+      print("Key: " + key.bytes.toString());
+      final encrypted = encrypter.encrypt(plainText, iv: iv);
+      print("Encrypted: " + encrypted.bytes.toString());
+      print("Base64: " + encrypted.base64);
+      return encrypted.base64;
+    }
+    return null;
   }
 
   String decrypt(String encryptedString) {
-    Encrypted encrypted = Encrypted.fromBase64(encryptedString);
-    final key = Key.fromUtf8(keyMain);
-    final iv = IV.fromUtf8(initVector);
-    final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
-    final decrypted = encrypter.decrypt(encrypted, iv: iv);
-    print("Decrypted: " + decrypted);
-    return decrypted;
+    if (encryptedString != null) {
+      Encrypted encrypted = Encrypted.fromBase64(encryptedString);
+      final key = Key.fromUtf8(keyMain);
+      final iv = IV.fromUtf8(initVector);
+      final encrypter = Encrypter(AES(key, mode: AESMode.cbc));
+      final decrypted = encrypter.decrypt(encrypted, iv: iv);
+      print("Decrypted: " + decrypted);
+      return decrypted;
+    }
+    return null;
   }
 }
