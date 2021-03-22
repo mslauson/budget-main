252856adc549577be19a69f651149e785d6de86b
matthewslauson <slauson.matthew@gmail.com>
2021-03-22 18:37:49 -0500

    create client for changing budgets

--- lib/client/budget_client.dart
+++ lib/client/budget_client.dart
@@ -5,7 +5,9 @@ import 'package:main/constants/budget_client_constants.dart';
 import 'package:main/constants/error_constants.dart';
 import 'package:main/error/error_handler.dart';
 import 'package:main/models/budget/getBudgetsResponse.dart';
+import 'package:main/models/budget/request/change_budget_request_model.dart';
 import 'package:main/models/budget/response/initialize_customer_categories_response.dart';
+import 'package:main/models/plaid/genericStatusResponseModel.dart';
 import 'package:main/security/blossom_encryption_utility.dart';
 import 'package:main/util/model_encryption_utility.dart';
 import 'package:main/util/uri_builder.dart';
@@ -45,5 +47,21 @@ class BudgetClient {
         InitializeCustomerCategoriesResponse.fromJson(
             jsonDecode(response.body));
     return getBudgetsResponse;
+  }
+
+  Future<GenericSuccessResponseModel> changeBudgetForTransaction(
+      String phone, ChangeBudgetRequestModel requestModel) async {
+    String encryptedPhone = _encryptionUtility.encrypt(phone);
+    encryptedPhone = Uri.encodeComponent(encryptedPhone);
+    String url = UriBuilder.blossomDevWithUri(BudgetClientConstants.URI_BUDGETS,
+        1, BudgetClientConstants.URI_PUT_CHANGE_CATEGORIES);
+    url = url + "?phone=" + encryptedPhone;
+    Response response = await put(url);
+    if (response.statusCode != 200 && response.statusCode != 404) {
+      ErrorHandler.onErrorClient(response, ErrorConstants.BUDGET_RETRIEVAL);
+    }
+    GenericSuccessResponseModel changeBudgetResponse =
+        GenericSuccessResponseModel.fromJson(jsonDecode(response.body));
+    return changeBudgetResponse;
   }
 }
--- lib/constants/budget_client_constants.dart
+++ lib/constants/budget_client_constants.dart
@@ -3,4 +3,5 @@ class BudgetClientConstants {
   static const URI_BUDGETS = "budgets";
   static const URI_GET_BUDGETS_MONTH = "phone/monthYear";
   static const URI_PUT_INITIALIZE_CATEGORIES = "categories/customer/initialize";
+  static const URI_PUT_CHANGE_CATEGORIES = "change";
 }
